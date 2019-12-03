458ebe656b438d38599ad807eaefcece12170cb7
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-03 16:52:34 -0700

    set up ski maps api set up nav set up get fetch to mountains getting all mountain names and ids on the front end set up google map api to display 1 map

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 9000
 const queries = require('./db/queries')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+app.use(bodyParser.json())
+app.use(cors())
 
 app.listen(port, () => console.log(`listening on ${port}`))
 
-app.get('/', (request, response) =>{
-    queries.getAll()
-        .then(users => response.send(users))
+app.get('/mountains', (request, response) =>{
+    queries.getAllMountain_Ids()
+        .then(mountains => response.send(mountains))
 })
