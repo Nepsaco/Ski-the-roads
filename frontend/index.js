b359e200af18a93a875ed6fbf5edee98f012bbed
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-04 13:58:58 -0700

    hero information displayed and styled

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,7 @@
 // const heroMap = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
 const id = 32
 const container = document.querySelector('#cardContainer')
+const hero = document.querySelector('#heroContainer')
 
 getMountainId()
     .then(mountains => mapMountains(mountains, createCard))
@@ -14,22 +15,53 @@ getMountainId()
 
 
 
-function appendElement(container, element){
-    container.append(element)
+function appendElement(container, ...element){
+    container.append(...element)
 }
 
 function createCard(mountain){
     const card = document.createElement('div')
+    const label = document.createElement('label')
     card.className = 'mountainCard'
-    card.textContent = mountain.name
+    label.textContent = mountain.name
+    label.id = mountain.mountain_id
     card.id = mountain.mountain_id
+
+    appendElement(card, label)
     appendElement(container, card)
     card.addEventListener('click', mountainCardClick)
     return card
 }
 
+function createHero(mountain){
+    const heroCard = document.createElement('div')
+    const h2 = document.createElement('h2')
+    const ul = document.createElement('ul')
+
+    heroCard.className = 'heroCard'
+    h2.textContent = mountain.name
+    ul.innerHTML = `<li>Lifts: ${mountain.lift_count}</li>
+    <li>Runs: ${mountain.run_count}</li>
+    <li>Annual Snowfall: ${mountain.annual_snowfall}</li>
+    <li>Skiable Acreage: ${mountain.skiable_acreage} acres</li>
+    <li>Website: <a href="${mountain.official_website}" target="_black">Click to visit</a></li>`
+
+
+    appendElement(hero, heroCard)
+    appendElement(heroCard, h2, ul)
+    return heroCard
+}
+
 function mountainCardClick(event){
-    return getMountainInfo(event.target.id)
+    getMountainInfo(event.target.id)
+        .then(createHero)
+        .then(clearHero)
+}
+
+function clearHero(element){
+    if(hero.childElementCount != 1){
+        hero.firstElementChild.remove() 
+    }
 }
 
 function mapMountains(mountains, definition){
