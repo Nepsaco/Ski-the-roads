ef92de684da78e4d304941c33de7f4ec14d8d063
Nepsaco <tobie.tsuzuki@gmail.com>
2019-12-04 08:47:16 -0700

    created mountain cards

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,11 @@
-const heroMap = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
+// const heroMap = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap`
 const id = 32
+const container = document.querySelector('#cardContainer')
 
 getMountainId()
-getMountainInfo(id)
-loadScript(heroMap)
-addMarker()
+    .then(mountains => mapMountains(mountains, createCard))
+// loadScript(heroMap)
+// addMarker()
 
 
 
@@ -13,7 +14,27 @@ addMarker()
 
 
 
+function appendElement(container, element){
+    container.append(element)
+}
 
+function createCard(mountain){
+    const card = document.createElement('div')
+    card.className = 'mountainCard'
+    card.textContent = mountain.name
+    card.id = mountain.mountain_id
+    appendElement(container, card)
+    card.addEventListener('click', mountainCardClick)
+    return card
+}
+
+function mountainCardClick(event){
+    return getMountainInfo(event.target.id)
+}
+
+function mapMountains(mountains, definition){
+    return mountains.map(definition)
+}
 
 function getMountainId(){
     return fetch('http://localhost:9000/mountains')
@@ -24,30 +45,30 @@ function handleResponse(response){
     return response.json()
 }
 
-function getMountainInfo(id){
-    return fetch(`https://cors-anywhere.herokuapp.com/https://skimap.org/SkiAreas/view/${id}.json`)
+function getMountainInfo(mountain_id){
+    return fetch(`https://cors-anywhere.herokuapp.com/https://skimap.org/SkiAreas/view/${mountain_id}.json`)
         .then(handleResponse)
 }
 
-function initMap(){
-    const colorado = {lat:39.113, lng:-105.358}
-    const mapProp = {
-        center: colorado,
-        zoom: 7
-    }
-    const map = new google.maps.Map(document.getElementById('map'), mapProp)
-}
+// function initMap(){
+//     const colorado = {lat:39.113, lng:-105.358}
+//     const mapProp = {
+//         center: colorado,
+//         zoom: 7
+//     }
+//     const map = new google.maps.Map(document.getElementById('map'), mapProp)
+// }
 
-function loadScript(src) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+// function loadScript(src) {
+//     let script = document.createElement('script');
+//     script.src = src;
+//     script.async = false;
+//     document.body.append(script);
+// }
 
-function addMarker(coordinates){
-    const marker = new google.maps.Marker({
-        position:coordinates,
-        map:map,
-    })
-}
+// function addMarker(coordinates){
+//     const marker = new google.maps.Marker({
+//         position:coordinates,
+//         map:map,
+//     })
+// }
